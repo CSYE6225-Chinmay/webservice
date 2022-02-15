e270ca7281b6a9b3490bb0ab84b0095a00f1db71
Chinmay Ashtikar <98785445+ChinmayAshtikar@users.noreply.github.com>
2022-02-15 18:38:56 -0500

    Update SpringbootrestApplication.java

--- src/main/java/com/cloud/SpringbootrestApplication.java
+++ src/main/java/com/cloud/SpringbootrestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootrestApplication {
+//public class SpringbootrestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootrestApplication.class, args);
+		SpringApplication.run(SpringbootrestApplication.class, args)
 	}
 
 }
