4e8c8c01b75ec4a41080807c69be531767acb12c
Chinmay Ashtikar <98785445+ChinmayAshtikar@users.noreply.github.com>
2022-04-15 03:03:18 -0400

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -6,46 +6,54 @@ const multer = require('multer');
 const dbConfig = require('../config/configDB.js');
 const logger = require("../config/logger");
 const SDC = require('statsd-client');
+const { route } = require('../index.js');
+const { Router } = require('express');
 const sdc = new SDC({host: dbConfig.METRICS_HOSTNAME, port: dbConfig.METRICS_PORT});
 var start = new Date();
 
 // GET Method
-
 router.get("/healthz", (req, res) => {
     console.log("Is it hitting?")
     sdc.timing('health.timeout', start);
-    logger.info("/health OK");
+    logger.info("/health running fine");
     sdc.increment('endpoint.health');
     res.sendStatus(200).json();
 });
 
-// POST Method
 
-router.post("/v2/user", userController.createUser);
+// POST Method
+router.post("/v1/user", userController.createUser);
+
 
 // GET Method (With Authentication)
+router.get("/v1/user/self", baseAuthentication(), userController.getUser);
 
-router.get("/v2/user/self", baseAuthentication(), userController.getUser);
 
 // PUT Method
+router.put("/v1/user/self", baseAuthentication(), userController.updateUser);
 
-router.put("/v2/user/self", baseAuthentication(), userController.updateUser);
 
 // Post Method for Picture
-
-
 const upload = multer({
     dest: 'uploads/'
 })
 
-router.post("/v2/user/self/pic", baseAuthentication(), upload.single('file'), imageController.updateUserPic);
+router.post("/v1/user/self/pic", baseAuthentication(), upload.single('file'), imageController.updateUserPic);
+
 
 // Get Picture
+router.get("/v1/user/self/pic", baseAuthentication(), imageController.getUserPic);
 
-router.get("/v2/user/self/pic", baseAuthentication(), imageController.getUserPic);
 
 // Delete Picture
+router.delete("/v1/user/self/pic", baseAuthentication(), imageController.deleteUserPic);
 
-router.delete("/v2/user/self/pic", baseAuthentication(), imageController.deleteUserPic);
+
+// Delete all User from user table
+router.delete("/v1/deleteAll", userController.deleteAllUser);
+
+
+// Verify User
+router.get("/v1/user/verifyUserEmail", userController.verifyUser);
 
 module.exports = router;
